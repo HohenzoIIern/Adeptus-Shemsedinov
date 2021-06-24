32057febbc83b4bb863b95dc66ecafd6c25162bd
HohenzoIIern <54852219+HohenzoIIern@users.noreply.github.com>
2021-06-24 07:30:08 +0300

    init

---
+++ UnitTest1.cs
@@ -0,0 +1,131 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lab2
+{
+    [TestClass]
+    public class BinaryFlagTesting
+    {
+        ulong minSize = 2;
+        ulong maxSize = 17179868704;
+        [TestMethod]
+        public void ConstructorOnMinSize()
+        {
+            new IIG.BinaryFlag.MultipleBinaryFlag(minSize);
+        }
+
+        [TestMethod]
+        public void ConstructorOverMinSize()
+        {
+            new IIG.BinaryFlag.MultipleBinaryFlag(minSize+1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ConstructorUnderMinSize()
+        {
+            new IIG.BinaryFlag.MultipleBinaryFlag(minSize-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ConstructorOverMaxSize()
+        {
+            new IIG.BinaryFlag.MultipleBinaryFlag(maxSize+1);
+        }
+
+        [TestMethod]
+        public void ConstructorUnderMaxSize()
+        {
+            new IIG.BinaryFlag.MultipleBinaryFlag(maxSize-1);
+        }
+
+        [TestMethod]
+        public void ConstructorOnMaxSize()
+        {
+            new IIG.BinaryFlag.MultipleBinaryFlag(maxSize);
+        }
+
+        [TestMethod]
+        public void ConstructorDefaultGetTrue()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(2);
+            Assert.IsTrue((bool)test.GetFlag());
+        }
+
+        public void ConstructorGetFalse()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(2, false);
+            Assert.IsFalse((bool)test.GetFlag());
+        }
+
+        [TestMethod]
+        public void SetGetTrue()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(2, false);
+            test.SetFlag(0);
+            test.SetFlag(1);
+            Assert.IsTrue((bool)test.GetFlag());
+        }
+
+        [TestMethod]
+        public void SetGetFalse()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(2, false);
+            test.SetFlag(0);
+            Assert.IsFalse((bool)test.GetFlag());
+        }
+
+        [TestMethod]
+        public void ResetOneGetFalse()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(3);
+            test.ResetFlag(1);
+            Assert.IsFalse((bool)test.GetFlag());
+        }
+
+        [TestMethod]
+        public void ResetAllGetFalse()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(3);
+            test.ResetFlag(0);
+            test.ResetFlag(1);
+            test.ResetFlag(2);
+            Assert.IsFalse((bool)test.GetFlag());
+        }
+
+        [TestMethod]
+        public void ResetSetGetTrue()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(3);
+            test.ResetFlag(1);
+            test.SetFlag(1);
+            Assert.IsTrue((bool)test.GetFlag());
+        }
+
+        [TestMethod]
+        public void Dispose()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(3);
+            test.Dispose();
+            Assert.AreEqual(null, test.GetFlag());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SetOverSize()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(2);
+            test.SetFlag(2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ResetOverSize()
+        {
+            var test = new IIG.BinaryFlag.MultipleBinaryFlag(2);
+            test.ResetFlag(2);
+        }
+    }
+}
+
